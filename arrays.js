a5f18e18967a5adc86e4fecb032449fb0a1f5c6c
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-06-06 23:41:30 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(arr, ele) {
-  arr.unshift(ele);
-  return ele;
+  [ele...arr];
+  return arr;
 }
 
 function destructivelyAddElementToBeginningOfArray (arr, ele) {
-  arr[1] = ele;
+  arr.unshift(ele);
   return ele;
 }
